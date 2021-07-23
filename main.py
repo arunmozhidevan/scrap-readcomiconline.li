d7d685a2bdaef1946626f37ccf040e14a8b518d7
AMD <arun.4devan@gmail.com>
2021-07-24 03:54:54 +0530

    main.py code quality increased

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
+"""csv,BeautifulSoup,requests,parse_information.py module."""
+import csv
 from bs4 import BeautifulSoup
 import requests
-import csv
 import parse_information as pi
+
 csv.register_dialect('semicolon-delimited', delimiter=';')
 
 
-def request_soup(URL):
-    r = requests.get(URL).text
-    soup = BeautifulSoup(r, 'lxml')
+def request_soup(url_holder):
+    """ Request HTML source from the page."""
+    response = requests.get(url_holder).text
+    soup = BeautifulSoup(response, 'lxml')
     return soup
 
 
-def page_content(URL):
-    tables = request_soup(URL).find_all('div', class_="col info")
+def page_content(url_holder, base_url_holder, comic_name_holder):
+    """ Calls the parse_information.py parse()
+        function to extract data from each links found on the page."""
+    tables = request_soup(url_holder).find_all('div', class_="col info")
     for table in tables:
+        new_url = base_url_holder + table.a['href']
         try:
             # csv_writer.writerow([table.a.text, Base_URL + table.a['href']])
-            NEW_URL = Base_URL + table.a['href']
+            str_val = (pi.parse(request_soup(new_url)))
+            print(f'{new_url} -> {str_val}')
+            with open(f"{comic_name_holder}-data.csv", 'w', newline='') as comic_book_data:
+                csv_writer_data = csv.writer(comic_book_data)
+                csv_writer_data.writerow([
+                    '''
+                    Name;Genres;Publisher;Writer;
+                    Artist;Publication date;Status;Views;url_link
+                    '''
+                ])
+                csv_writer_data.writerow([str_val + ';' + new_url])
+        except RuntimeError as error:
+            print(error)
+            print(f'Error: {new_url}')
 
-            str_val = (pi.parse(request_soup(NEW_URL)))
-            print(f'{NEW_URL} -> {str_val}')
-            csv_writer_data.writerow([str_val +';'+ NEW_URL])
-        except Exception as e:
-            print(e)
-            print(f'Error: {NEW_URL}')
-            pass
+
+def main():
+    """ Main function."""
+    url_link = 'https://readcomiconline.li/Publisher/Archie-Comics'
+    comic_name = url_link.split('/')[4]
+    base_name = url_link.split('/')[2]
+    base_url = f'https://{base_name}'
+    with open(f"{comic_name}-data.csv", 'w', newline='') as comic_book_data:
+        csv_writer_data = csv.writer(comic_book_data)
+        csv_writer_data.writerow([
+            '''
+                Name;Genres;Publisher;Writer;
+                Artist;Publication date;Status;Views;url_link
+                '''
+        ])
+
+    while True:
+        pages = request_soup(url_link).find('a', class_="right_bt next_bt")
+        page_content(url_link, base_url, comic_name)
+        try:
+            next_url = pages['href']
+            url_link = base_url + next_url
+
+        except RuntimeError as error:
+            print(f'Error: {error}\n End of page reached')
+            break
+    comic_book_data.close()
 
 
 if __name__ == '__main__':
-
-    URL = 'https://readcomiconline.li/Publisher/Archie-Comics'
-    comic_name = URL.split('/')[4]
-    Base_name = URL.split('/')[2]
-    Base_URL = f'https://{Base_name}'
-
-    comic_book_data = open(f"{comic_name}-data.csv", 'w', newline='')
-    csv_writer_data = csv.writer(comic_book_data)
-    csv_writer_data.writerow(['Name;Genres;Publisher;Writer;Artist;Publication date;Status;Views;URL'])
-
-    while True:
-        pages = request_soup(URL).find('a', class_="right_bt next_bt")
-        page_content(URL)
-        try:
-            next_url = pages['href']
-            URL = Base_URL + next_url
-
-        except Exception as e:
-            print(f'Error: {e}\n End of page reached')
-            break
+    main()
