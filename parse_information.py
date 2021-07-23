fea1e65fb964e03f7f99c2c12c673d3418e8ae60
AMD <arun.4devan@gmail.com>
2021-07-24 04:03:33 +0530

    parse_information.py code quality increased

--- parse_information.py
+++ parse_information.py
@@ -1,24 +1,23 @@
+"""BeautifulSoup module."""
 from bs4 import BeautifulSoup
 
 
 def parse(file_soup):
-    # file_soup = open('queue.txt', 'r')    delete this later
+    """Takes the HTML tags input and parse the 'div'
+    and 'a' information from it."""
     soup = file_soup
-
     file_list = []
-
     title = soup.find('div', class_="heading")
     file_list.append(title.text)
-    p = soup.find('div', class_="col info")
+    response = soup.find('div', class_="col info")
 
     for i in range(1, 8):
-        value = (p.select(f'p:nth-of-type({i})'))
+        value = (response.select(f'p:nth-of-type({i})'))
         soup = BeautifulSoup(str(value[0]), 'lxml')
-
         list1 = []
+
         for tag in soup.find_all('a', class_="dotUnder"):
             list1.append(tag.text)
-
         if i < 5:
             file_list.append(', '.join(list1))
         else:
